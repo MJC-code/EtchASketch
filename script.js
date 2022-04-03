fccc20fd50779939aa80480441af886f24118d33
Mark Cookson <cookson.mark@gmail.com>
2022-04-03 22:26:26 +1200

    Programatically add hover event listener to every div

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const container = document.getElementById('container');
 
-const numberRows = 4;
+const numberRows = 25;
+const numberBoxes = numberRows * numberRows;
 
 function fillWithDivs(numberRows) {
-    for (let i = 0; i < numberRows*numberRows; i++) {
+    for (let i = 0; i < numberBoxes; i++) {
         container.innerHTML += `<div id=${i}></div>`;
     }
 }
@@ -13,7 +14,11 @@ fillWithDivs(numberRows);
 container.style.gridTemplateColumns = `repeat(${numberRows}, auto)`
 container.style.gridTemplateRows = `repeat(${numberRows}, auto)`
 
-const target = document.getElementById('1');
 
+for (let i = 0; i < numberBoxes; i++ ) {
+let target = document.getElementById(`${i}`);
 target.addEventListener('mouseenter', e => {
     target.style.background = 'black';})
+
+
+}
