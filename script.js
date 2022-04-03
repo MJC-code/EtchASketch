6d567b591d5dce74eb368d0cd9b2fd55177c5c64
Mark Cookson <cookson.mark@gmail.com>
2022-04-03 21:50:38 +1200

    Allow setting number of rows and columns programatically

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const container = document.getElementById('container');
 
-const numberRows = 7;
+const numberRows = 16;
 
 container.style.gridTemplateColumns = `repeat(${numberRows}), 1fr)`;
 container.style.gridTemplateRows = `repeat(${numberRows}), 1fr)`;
 
 
 function fillWithDivs(numberRows) {
-    for (let i = 0; i < 49; i++) {
+    for (let i = 0; i < numberRows*numberRows; i++) {
         container.innerHTML += `<div></div>`;
     }
 }
 
 
 
-fillWithDivs(4);
-
+fillWithDivs(numberRows);
+container.style.gridTemplateColumns = `repeat(${numberRows}, auto)`
+container.style.gridTemplateRows = `repeat(${numberRows}, auto)`
