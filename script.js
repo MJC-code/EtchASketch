05c8908c36c77445f2815a9e97fd476cbf2a4ae2
Mark Cookson <cookson.mark@gmail.com>
2022-04-03 23:01:38 +1200

    Add code to clear all divs from container when 'Create New Grid' button pushed

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 const container = document.getElementById('container');
+const newGridButton = document.getElementById('newGridButton')
+newGridButton.addEventListener('click', newGrid);
 
-const numberRows = 25;
-const numberBoxes = numberRows * numberRows;
 
-function fillWithDivs(numberRows) {
+function fillWithDivs(numberBoxes) {
     for (let i = 0; i < numberBoxes; i++) {
-        container.innerHTML += `<div id=${i}></div>`;
+        container.innerHTML += `<div class='cell' id=${i}></div>`;
     }
 }
 
 
-fillWithDivs(numberRows);
-container.style.gridTemplateColumns = `repeat(${numberRows}, auto)`
-container.style.gridTemplateRows = `repeat(${numberRows}, auto)`
 
 
-for (let i = 0; i < numberBoxes; i++ ) {
-let target = document.getElementById(`${i}`);
-target.addEventListener('mouseenter', e => {
-    target.style.background = 'black';})
 
 
+function drawGrid(size) {
+    const numberRows = size;
+    const numberBoxes = numberRows * numberRows;
+
+    fillWithDivs(numberBoxes);
+
+    container.style.gridTemplateColumns = `repeat(${numberRows}, auto)`
+    container.style.gridTemplateRows = `repeat(${numberRows}, auto)`
+
+    for (let i = 0; i < numberBoxes; i++) {
+        let target = document.getElementById(`${i}`);
+        target.addEventListener('mouseenter', e => {
+            target.style.background = 'black';
+        })
+    }
 }
+
+
+function newGrid() {
+   while (container.firstChild) container.removeChild (container.firstChild);
+   
+    drawGrid(4);
+}
+
+drawGrid(16)
