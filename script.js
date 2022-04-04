ad4ecfd7054bb17f04d1ab58f1c18b8a3a0e7f44
Mark Cookson <cookson.mark@gmail.com>
2022-04-04 22:27:03 +1200

    Add Change Colour Style button to change colourStyle variable

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const container = document.getElementById('container');
 const newGridButton = document.getElementById('newGridButton')
 newGridButton.addEventListener('click', newGrid);
-
+const colourButton = document.getElementById('setColourStyle')
+colourButton.addEventListener('click', changeColourStyle)
+let colourStyle = 'black';
 
 function newGrid() {
     let numberRows = prompt("How many squares per side in the new grid? (from 2 to 100)")
-    if (numberRows == null)  return;
+    if (numberRows == null) return;
     if (numberRows < 2) numberRows = 2;
     if (numberRows > 100) numberRows = 100;
     container.innerHTML = '';
@@ -16,15 +18,17 @@ function newGrid() {
 function drawGrid(size) {
     const numberRows = size;
     const numberBoxes = numberRows * numberRows;
-    
+
     fillWithDivs(numberBoxes);
+
     container.style.gridTemplateColumns = `repeat(${numberRows}, auto)`
     container.style.gridTemplateRows = `repeat(${numberRows}, auto)`
 
     for (let i = 0; i < numberBoxes; i++) {
         let target = document.getElementById(`${i}`);
-        target.addEventListener('mouseenter', e => {
-            target.style.background = 'black';
+        target.addEventListener('mouseenter', () => {
+            // target.style.background = 'black';
+            setBackgroundColour(target)
         })
     }
 }
@@ -37,5 +41,22 @@ function fillWithDivs(numberBoxes) {
         container.appendChild(child);
     }
 }
+
+function setBackgroundColour(target) {
+    target.style.background = 'black';
+    return;
+}
+
+function changeColourStyle() {
+    if (colourStyle === 'black') {
+        colourStyle = 'shade'
+    } else if (colourStyle === 'shade') {
+        colourStyle = 'multi'
+    } else if (colourStyle === 'multi') {
+        colourStyle = 'black'
+    }
+    console.log(`Colour style changed to ${colourStyle}`)
+}
+
 
 drawGrid(16)
